76294360ddb37dd74da8649ed60b21455ad88794
yash565-git0 <012yash789@gmail.com>
2025-06-23 16:02:32 +0530

    database connected

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import mongoose from "mongoose"
+// import { DB_Name } from "./constants"
+// import express from "express"
+// const app = express()
+// ;(async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+//         app.on("error",(error)=>{
+//             console.log("there is some error ");
+//             throw error
+//          })
+//          app.listen(process.env.PORT , ()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`);
+            
+//          })
+        
+//     } catch (error) {
+//         console.log("ERROR : ",error)
+//         throw err
+//     }
+
+// })()
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: '/.env'
+})
+connectDB()
+
