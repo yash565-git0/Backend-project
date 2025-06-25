0b6310515c9a1087f6bbadf572af86ae1092877f
yash565-git0 <012yash789@gmail.com>
2025-06-25 17:26:54 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,66 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const registerUser = asyncHandler( async(req,res)=>{
-    res.status(200).json({
-        message : "ok",
-    })
+    
+const {username,email,FullName,password,} = req.body
+console.log("email : ",email);
+
+if ([FullName,username,email,password].some((field)=> field?.trim === "")) {
+    throw new ApiError(400,"All fields are required")
+}
+
+const existedUser = User.findOne({
+    $or:[{username},{email}]
 })
+if (existedUser) {
+    throw new ApiError(409,"User with such email/Username already exists")
+}
+const avatarLocalpath = req.files?.avatar[0]?.path;
+const coverImageLocalpath = req.files?.coverImage[0]?.path
+
+if (!avatarLocalpath){
+    throw new ApiError(400,"Avatar file is required")
+}
+const avatar = await uploadOnCloudinary(avatarLocalpath)
+const coverImage = await uploadOnCloudinary(coverImageLocalpath)
+
+if (!avatar){
+    throw new ApiError(400,"Avatar file is required")
+}
+const user = await User.create({
+    FullName,
+    avatar : avatar.url,
+    coverImage: coverImage?.url || "" ,
+    email,password,
+    username: username.toLowerCase()
+})
+
+const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+if(!createdUser){
+    throw new ApiError(500,"Something went wrong while registering the user")
+}
+
+return res.status(201).json(
+    new  ApiResponse(200,createdUser,"User registered successfully")
+)
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,24 +34,25 @@ const userSchema =  new Schema({
      ],
      avatar : {
         type : String,
+        required: true
 
      },
      coverImage : {
         type:String
      },
-          password : {
-            type:String,
-            required : [true,"Password is  required"]
+     password : {
+        type:String,
+        required : [true,"Password is  required"]
      },
-          refreshToken : {
-            type:String
+    refreshToken : {
+        type:String
      },
 
 },
 {timestamps:true})
 userSchema.pre("save", async function(next){
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 userSchema.methods.isPasswordCorrect() = async function (password) {
@@ -82,4 +83,7 @@ userSchema.methods.generateRefreshToken()=function(){
 
 )
 }
+
+
+
 export const User = mongoose.model("User",userSchema)
