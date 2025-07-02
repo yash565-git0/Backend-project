71723e4fa5abe236f9c040e2cf565aa495b208b8
yash565-git0 <012yash789@gmail.com>
2025-07-02 12:50:06 +0530

    did some debugging

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser,registerUser , logoutUser , refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
+import { loginUser,
+        registerUser , 
+        logoutUser , 
+        refreshAccessToken, 
+        changeCurrentPassword,
+        getCurrentUser, 
+        updateAccountDetails, 
+        updateUserAvatar, 
+        updateUserCoverImage,
+        getUserChannelProfile, 
+        getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -37,7 +47,7 @@ router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
 
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/c/:useranme").get(verifyJWT,getUserChannelProfile)
 
